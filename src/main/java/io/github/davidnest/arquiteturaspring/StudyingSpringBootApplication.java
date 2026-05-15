9d81b98d26eb2bf876b38cb2d537b610b817a466
David Nest <davidcamposoliveira80@gmail.com>
2026-05-15 10:54:00 -0300

    First Commit

---
+++ src/main/java/io/github/davidnest/arquiteturaspring/StudyingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.github.davidnest.arquiteturaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyingSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyingSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/davidnest/arquiteturaspring/StudyingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.davidnest.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
